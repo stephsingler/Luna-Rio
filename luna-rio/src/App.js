2ea0aca81aac60c14a1111615d722c33a8170bfe
stephsingler <singlerstephanie@gmail.com>
2018-01-23 21:51:35 -0600

    test commit

---
+++ luna-rio/src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './index.css';
+
+class App extends Component {
+  render() {
+    return (
+      <h1>Welcome to Luna Rio!</h1>
+    );
+  }
+}
+
+export default App;
